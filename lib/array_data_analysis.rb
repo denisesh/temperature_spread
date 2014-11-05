73a4f00fe3eb9a462a9263781fed42785a08eae7
--denisesh <--denisesh@gmail.com>
2014-11-05 11:20:41 -0800

    all but average_temperatures

--- lib/array_data_analysis.rb
+++ lib/array_data_analysis.rb
@@ -29,7 +29,7 @@ class ArrayDataAnalysis
 
   def average_temperatures
     avg_temperatures = []
-    avg_temperatures.map 
+
     #average_temperatures = @data.each |t| { t[1] << t[2]}
 
     #"returns a array of average temperatures, on for each month"
@@ -37,11 +37,10 @@ class ArrayDataAnalysis
     #expect(analyzer.average_temperatures[0]).to be == 73.5
     #expect(analyzer.average_temperatures[29]).to be == 67.5
     # create an array called average_temperatures with a length of 30
-  #   analyzer = TEMPERATURE_DATA.each do |ary|
-  #     @data.each do |t|
-  #       avg_month << (t[1] + t[2]/2).to_f
-  #     end
-  #   end
-  #
+    TEMPERATURE_DATA.each do |t|
+         average_temperatures << ((t[1] + t[2]/2)).to_f
+       end
+
+
   end
 end
